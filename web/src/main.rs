de6a3e082b3375d076be55f651e9276a339dba91
Po Huit <eve.po.huit@gmail.com>
2018-05-04 01:00:47 -0700

    started to build webserver

--- web/src/main.rs
+++ web/src/main.rs
@@ -6,16 +6,32 @@
 // Plan B: EVE route planner with options
 // Web client
 
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+use rocket::request::*;
+
+extern crate plan_b;
+use plan_b::*;
+
+#[derive(FromForm)]
+struct RouteSpec {
+    from: String,
+    to: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn front_page() -> &'static str {
+    "Plan B"
+}
+
+#[post("/route", data = "<route_spec>")]
+fn search_route(route_spec: Form<RouteSpec>) -> String {
+    let route_spec = route_spec.get();
+    format!("{} -> {}", route_spec.from, route_spec.to)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![front_page, search_route]).launch();
 }
